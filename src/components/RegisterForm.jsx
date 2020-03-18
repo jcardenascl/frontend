50b9f44eb356a9b9302d963161c6c04467bb1909
Daniel Esteves <estevesd8@gmail.com>
2020-03-18 18:07:03 -0400

    Google button working properly

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -2,6 +2,7 @@
 import React, { useState, useContext, useEffect } from 'react';
 import propTypes from 'prop-types';
 import { redirectTo } from 'fogg-utils';
+import { GoogleLogin } from 'react-google-login';
 
 // Contexts
 import { FormContext } from '@contexts/form';
@@ -21,6 +22,11 @@ const RegisterForm = ({ currentUrl }) => {
   const { handleInputChange, values } = useContext(FormContext);
   const { createUser } = useContext(UserContext);
 
+  // Effects
+  useEffect(() => {
+    if (!ready) setReady(true);
+  }, [ready]);
+
   // Methods
   const handleRegister = async user => {
     const response = await createUser(user);
@@ -33,19 +39,86 @@ const RegisterForm = ({ currentUrl }) => {
     }
   };
 
-  // Effects
-  useEffect(() => {
-    if (!ready) setReady(true);
-  }, [ready]);
+  const responseGoogle = response => {
+    console.log(response);
+  };
 
   return (
     <div className="container">
       <div className="flex justify-center">
-        <div className="w-full mt-16 transition-all duration-200 bg-white sm:mt-24 lg:mt-32 sm:w-3/4 md:w-4/5 lg:w-2/5">
+        <div className="w-full mt-16 transition-all duration-200 bg-white sm:mt-24 lg:mt-32 sm:w-3/4 lg:w-5/12">
           <div className="px-12 py-6 rounded-lg shadow-md">
             <h1 className="pb-2 mb-6 text-4xl font-bold text-center text-gray-700 border-b">
               Register
             </h1>
+
+            <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
+              <GoogleLogin
+                clientId="1021304788868-4bfpvgk7dqfo10e56k1io56r517nknmi.apps.googleusercontent.com"
+                render={renderProps => (
+                  <button
+                    type="button"
+                    className="flex items-center justify-center w-full px-4 py-3 text-xs font-semibold tracking-wider text-black uppercase bg-white border rounded"
+                    onClick={renderProps.onClick}
+                    disabled={renderProps.disabled}
+                  >
+                    <svg className="w-4 h-4 mr-2" viewBox="0 0 533.5 544.3">
+                      <path
+                        d="M533.5 278.4c0-18.5-1.5-37.1-4.7-55.3H272.1v104.8h147c-6.1 33.8-25.7 63.7-54.4 82.7v68h87.7c51.5-47.4 81.1-117.4 81.1-200.2z"
+                        fill="#4285f4"
+                      />
+                      <path
+                        d="M272.1 544.3c73.4 0 135.3-24.1 180.4-65.7l-87.7-68c-24.4 16.6-55.9 26-92.6 26-71 0-131.2-47.9-152.8-112.3H28.9v70.1c46.2 91.9 140.3 149.9 243.2 149.9z"
+                        fill="#34a853"
+                      />
+                      <path
+                        d="M119.3 324.3c-11.4-33.8-11.4-70.4 0-104.2V150H28.9c-38.6 76.9-38.6 167.5 0 244.4l90.4-70.1z"
+                        fill="#fbbc04"
+                      />
+                      <path
+                        d="M272.1 107.7c38.8-.6 76.3 14 104.4 40.8l77.7-77.7C405 24.6 339.7-.8 272.1 0 169.2 0 75.1 58 28.9 150l90.4 70.1c21.5-64.5 81.8-112.4 152.8-112.4z"
+                        fill="#ea4335"
+                      />
+                    </svg>
+                    Google
+                  </button>
+                )}
+                buttonText="Login"
+                onSuccess={res => responseGoogle(res)}
+                onFailure={res => responseGoogle(res)}
+                cookiePolicy="single_host_origin"
+              />
+
+              <button
+                type="button"
+                className="flex items-center justify-center w-full px-4 py-3 text-xs font-semibold tracking-wider text-white uppercase rounded facebook"
+              >
+                <svg
+                  className="w-4 h-4 mr-2"
+                  fill="currentColor"
+                  viewBox="0 0 155.139 155.139"
+                >
+                  <path d="M89.584 155.139V84.378h23.742l3.562-27.585H89.584V39.184c0-7.984 2.208-13.425 13.67-13.425l14.595-.006V1.08C115.325.752 106.661 0 96.577 0 75.52 0 61.104 12.853 61.104 36.452v20.341H37.29v27.585h23.814v70.761h28.48z" />
+                </svg>
+                Facebook
+              </button>
+
+              <button
+                type="button"
+                className="flex items-center justify-center w-full px-4 py-3 text-xs font-semibold tracking-wider text-white uppercase rounded twitter"
+              >
+                <svg
+                  className="w-4 h-4 mr-2"
+                  fill="currentColor"
+                  viewBox="0 0 512 512"
+                >
+                  <path d="M512 97.248c-19.04 8.352-39.328 13.888-60.48 16.576 21.76-12.992 38.368-33.408 46.176-58.016-20.288 12.096-42.688 20.64-66.56 25.408C411.872 60.704 384.416 48 354.464 48c-58.112 0-104.896 47.168-104.896 104.992 0 8.32.704 16.32 2.432 23.936-87.264-4.256-164.48-46.08-216.352-109.792-9.056 15.712-14.368 33.696-14.368 53.056 0 36.352 18.72 68.576 46.624 87.232-16.864-.32-33.408-5.216-47.424-12.928v1.152c0 51.008 36.384 93.376 84.096 103.136-8.544 2.336-17.856 3.456-27.52 3.456-6.72 0-13.504-.384-19.872-1.792 13.6 41.568 52.192 72.128 98.08 73.12-35.712 27.936-81.056 44.768-130.144 44.768-8.608 0-16.864-.384-25.12-1.44C46.496 446.88 101.6 464 161.024 464c193.152 0 298.752-160 298.752-298.688 0-4.64-.16-9.12-.384-13.568 20.832-14.784 38.336-33.248 52.608-54.496z" />
+                </svg>
+                Twitter
+              </button>
+            </div>
+
+            <hr className="my-6" />
 
             <Alert
               message={errorMessage}
